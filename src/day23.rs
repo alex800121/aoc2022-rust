b2376b98cd8a2782b85b9bd9063004f9e2ddf977
alex800121 <alex800121@hotmail.com>
2023-08-09 19:05:56 +0000

    20230810

--- src/day23.rs
+++ src/day23.rs
@@ -4,9 +4,44 @@ use aoc2022::{
     Enum,
 };
 use project_root::get_project_root;
-use std::collections::BTreeMap;
+use std::{collections::{BTreeMap, BTreeSet}, array::from_fn};
 
 type Index = (isize, isize);
+type Elves = BTreeMap<Index, Index>;
+type Forbidden = BTreeSet<Index>;
+
+const ADJACENT: [Index; 8] = [
+    (-1, -1), (0, -1), (1, -1),
+    (-1, 0), (1, 0),
+    (-1, 1), (0, 1), (1, 1)
+];
+
+const PROPOSAL: [[Index; 3]; 4] = [
+    [(-1, -1), (0, -1), (1, -1)],
+    [(-1, 1), (0, 1), (1, 1)],
+    [(-1, -1), (-1, 0), (-1, 0)],
+    [(1, -1), (1, 0), (1, 0)],
+];
+
+fn propose_move(elves: &mut Elves, step: usize) {
+    let mut forbidden: Forbidden = BTreeSet::new();
+    let mut next_moves: Elves = BTreeMap::new();
+    let proposal: [[Index; 3]; 4] = from_fn(|n| PROPOSAL[n % 4]);
+    'outer: for (ori, _) in elves.iter() {
+        if ADJACENT.iter().all(|x| !elves.contains_key(&(x.0 + ori.0, x.1 + ori.1))) {
+            next_moves.insert(*ori, *ori);
+        } else {
+            'inner: for direction in proposal {
+                let direction = direction.map(|x| (x.0 + ori.0, x.1 + ori.1));
+                let proposed = direction[1];
+                if direction.iter().all(|x| !elves.contains_key(x)) {
+                    if let Some(duplicate_ori) = next_moves.get(&proposed) {
+                    }
+                }
+            }
+        }
+    }
+}
 
 pub fn run(input: usize) {
     let input = std::fs::read_to_string(format!(
